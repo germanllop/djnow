aea8b42981e53a744b4be535e30e04fb383f9dd3
German Llop <germanllop@gmail.com>
2020-05-28 16:42:29 -0500

    ig scope user profile

--- routes/auth.js
+++ routes/auth.js
@@ -21,7 +21,7 @@ router.get('/facebook/callback',
 
 // Instagram auth routes
 router.get('/instagram',
-  passport.authenticate('instagram',{ scope: ['instagram_basic'] }))
+  passport.authenticate('instagram',{ scope: ['user_profile'] }))
 
 router.get('/instagram/callback', 
   passport.authenticate('instagram', { failureRedirect: process.env.BASE_URL }),
