e5d45e4ce2af01f31636b4ac9b1c274491a0adfe
German Llop <germanllop@gmail.com>
2020-07-22 01:41:36 -0500

    shadows and categories

--- models/category.js
+++ models/category.js
@@ -1,9 +1,17 @@
 const mongoose = require('mongoose')
+const slug = require('mongoose-slug-generator')
+mongoose.plugin(slug)
 
 const categorySchema = new mongoose.Schema({
   name: {
     type: String,
     default: ""
+  },
+  slug: { 
+    type: String, 
+    slug: "name",
+    slug_padding_size: 1, 
+    unique: true 
   },
   subtitle: {
     type:String,
